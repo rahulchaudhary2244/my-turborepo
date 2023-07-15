5200dac4cc4250fcf7b99785a6fd01bf554cf88e
rahulchaudhary2244 <rahulchaudhary2244@gmail.com>
2023-07-15 14:11:12 +0530

    storybook setup for turporepo

--- packages/ui/Button.tsx
+++ packages/ui/Button.tsx
@@ -1,7 +1,24 @@
 "use client";
 
-import * as React from "react";
-
-export const Button = () => {
-  return <button onClick={() => alert("boop")}>Boop</button>;
+interface Props {
+  primary?: boolean;
+  size?: "small" | "large";
+  label?: string;
+}
+ 
+export const Button = ({
+  primary = false,
+  label = "Boop",
+  size = "small",
+}: Props) => {
+  return (
+    <button
+      style={{
+        backgroundColor: primary ? "red" : "blue",
+        fontSize: size === "large" ? "24px" : "14px",
+      }}
+    >
+      {label}
+    </button>
+  );
 };
